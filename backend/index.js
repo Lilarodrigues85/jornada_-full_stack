cd2c28a0c72f1194ce39b7ccb08757c18df96810
Lila <dalila.analistadesistema@gmail.com>
2022-08-11 09:12:32 -0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
 app.get("/", function (req, res) {
-  res.send("Hello, World!");
-});
+  res.send("Hello, World!")
+})
 
 app.get("/oi", function (req, res) {
-  res.send("Olá, mundo!");
-});
+  res.send("Olá, mundo!")
+})
 
 app.listen(3000);
