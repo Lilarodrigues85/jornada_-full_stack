16d7b1f36ce4f241a817e9b9562e7795b0e8f2a5
Lila <dalila.analistadesistema@gmail.com>
2022-08-22 12:08:50 -0300

    Continuação

--- frontend/src/Componentes/Jogo/Jogo.js
+++ frontend/src/Componentes/Jogo/Jogo.js
@@ -1,7 +1,10 @@
 import "./Jogo.css";
+import clouds from "../../assets/clouds.png";
 
 function jogo () {
-    return <div>Jogo</div>;
+    return <div className="jogo">
+        <img className="nuvens" alt="nuvens" src={clouds}/>
+    </div>;
 }
 
 export default jogo;
