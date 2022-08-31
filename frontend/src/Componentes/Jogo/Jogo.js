861fd3fa52e75a2829b218696564d8e415e18b10
Lila <dalila.analistadesistema@gmail.com>
2022-08-31 09:44:57 -0300

    continuação

--- frontend/src/Componentes/Jogo/Jogo.js
+++ frontend/src/Componentes/Jogo/Jogo.js
@@ -2,11 +2,13 @@ import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
 import pipe from "../../assets/pipe.png";
 import mario from "../../assets/mario.gif";
+import gameOver from "../../assets/game-over.png";
 import { useRef, useState } from "react";
 
 function Jogo () {
     
     const [estaPulando, setEstaPulando] = useState(false);
+    const [estaMorto, setEstaMorto] = useState(false)
 
     const marioRef = useRef();
     const canoRef = useRef();
@@ -31,9 +33,13 @@ function Jogo () {
 // Implementação temporária para exibir se o mário está no cano
   // ou não
   setInterval(function () {
-    const valor = marioEstaNoCano();
+   const estaNoCano = marioEstaNoCano();
 
-    console.log("Mário está no cano?", valor);
+   if (!estaNoCano){
+    return;
+   }
+
+  setEstaMorto(true);
   }, 100);
 
 
@@ -51,15 +57,15 @@ function Jogo () {
         marioClassName = "mario mario-pulo";
     }
   
-
-    console.log(15, {estaPulando});
+    const marioImage = estaMorto ? gameOver : mario;
+  
 
     return <div className="jogo">
         <img className="nuvens" alt="nuvens" src={clouds}/>
 
         <img ref={canoRef} className="pipe" alt="pipe" src={pipe}/>
 
-        <img ref={marioRef} className={marioClassName} src={mario} alt="mario"/>
+        <img ref={marioRef} className={marioClassName} src={marioImage} alt="mario"/>
 
         <div className="chao"></div>
 
